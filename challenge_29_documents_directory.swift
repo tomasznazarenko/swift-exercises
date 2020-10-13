238c52cbf3c7558f6c159af48475f04df4bee8b8
Tomasz Nazarenko <tomasz.nazarenko@icloud.com>
2020-10-13 19:15:42 +0200

    Create challenge_29_documents_directory.swift

---
+++ challenge_29_documents_directory.swift
@@ -0,0 +1,8 @@
+// Write a function that returns a URL to the user’s documents directory.
+
+import Foundation
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
